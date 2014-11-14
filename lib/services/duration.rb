bfc516f965d8803a2852f34812ab71c4cc92a8b9
Scott Muc <scottmuc@gmail.com>
2014-11-14 19:47:26 +0530

    refactored to strategy pattern, supports 62:33
    * freakonomics had durations formed as 65:33 so using dates didn't work
    * strategy pattern will assist in adding other odd parse requirements
    * could refactor to single strategy and construct with blocks

--- lib/services/duration.rb
+++ lib/services/duration.rb
@@ -1,21 +1,54 @@
+
+class SecondsParser
+  def can_parse(string)
+    string.split(':').count == 1
+  end
+
+  def parse(string)
+    Duration.new(string.to_i / 60)
+  end
+end
+
+class MinutesSecondsParser
+  def can_parse(string)
+    string.split(':').count == 2
+  end
+
+  def parse(string)
+    minutes = string.split(':')[0].to_i
+    Duration.new(minutes)
+  end
+end
+
+class HoursMinutesSecondsParser
+  MIDNIGHT = Time.parse('00:00:00')
+
+  def can_parse(string)
+    string.split(':').count == 3
+  end
+
+  def parse(string)
+    duration = Time.parse(string) - MIDNIGHT
+    Duration.new(duration.to_i / 60)
+  end
+end
+
 class Duration
   attr_reader :minutes
 
-  MIDNIGHT = Time.parse('00:00:00')
+  BUILT_IN_PARSERS = [
+    SecondsParser.new,
+    MinutesSecondsParser.new,
+    HoursMinutesSecondsParser.new,
+  ]
 
   def initialize(minutes)
     @minutes = minutes
   end
 
-  def self.parse(string)
-    if string.split(':').count == 1
-      return Duration.new(string.to_i / 60)
-    end
-    if string.split(':').count == 2
-      string = '00:' + string
-    end
-    duration = Time.parse(string) - MIDNIGHT
-    Duration.new(duration.to_i / 60)
+  def self.parse(string, parsers=BUILT_IN_PARSERS)
+    parser = parsers.find { |parser| parser.can_parse string }
+    parser.parse string
   end
 end
 
--- spec/service/duration_spec.rb
+++ spec/service/duration_spec.rb
@@ -15,5 +15,53 @@ describe Duration do
     duration = Duration.parse("120")
     expect( duration.minutes ).to eq(2)
   end
+
+  it "interacts with a parser to obtain a duration" do
+    parser = double(:parser)
+    expect(parser).to receive(:can_parse).with("120") { true }
+    expect(parser).to receive(:parse).with("120")
+    duration = Duration.parse("120", [parser])
+  end
+end
+
+describe SecondsParser do
+  it "can parse time formated as a number" do
+    parser = SecondsParser.new
+    expect(parser.can_parse("120")).to eq true
+  end
+
+  it "parses 120 as a 2 minute duration" do
+    parser = SecondsParser.new
+    expect(parser.parse("120").minutes).to eq 2
+  end
+end
+
+describe MinutesSecondsParser do
+  it "can parse time formated as MM:SS" do
+    parser = MinutesSecondsParser.new
+    expect(parser.can_parse("45:33")).to eq true
+  end
+
+  it "parses 45:33 as a 45 minute duration" do
+    parser = MinutesSecondsParser.new
+    expect(parser.parse("45:33").minutes).to eq 45
+  end
+
+  it "parses 62:33 as a 62 minute duration" do
+    parser = MinutesSecondsParser.new
+    expect(parser.parse("62:33").minutes).to eq 62
+  end
+end
+
+describe HoursMinutesSecondsParser do
+  it "can parse time formated as HH:MM:SS" do
+    parser = HoursMinutesSecondsParser.new
+    expect(parser.can_parse("01:45:33")).to eq true
+  end
+
+  it "parses 01:45:33 as a 105 minute duration" do
+    parser = HoursMinutesSecondsParser.new
+    expect(parser.parse("01:45:33").minutes).to eq 105
+  end
 end
 
