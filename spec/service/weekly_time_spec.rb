cb31ac2771ac198121798c08e69b9d94cd581157
Scott Muc <scottmuc@gmail.com>
2014-11-14 16:26:18 +0530

    cleaned up tests, not sure if it's an improvement

--- spec/service/weekly_time_spec.rb
+++ spec/service/weekly_time_spec.rb
@@ -1,23 +1,19 @@
 require_relative '../../lib/services/weekly_time'
 
 describe WeeklyTime do
+  def stub_feed_stats(stats)
+    expect(FeedStats).to receive(:for) { stats }
+  end
+
+  let(:time) { WeeklyTime.for "some feed" }
+
   it "reports around 60 minutes for the average podcast" do
-    expect(FeedStats).to receive(:for) {
-      { :release_cadence => 7,
-        :average_length => 60
-      }
-    }
-    time = WeeklyTime.for "some feed"
+    stub_feed_stats({ :release_cadence => 7, :average_length => 60 })
     expect(time).to eq(60)
   end
 
   it "reports little time for infrequently released podcasts" do
-    expect(FeedStats).to receive(:for) {
-      { :release_cadence => 30,
-        :average_length => 45
-      }
-    }
-    time = WeeklyTime.for "some feed"
+    stub_feed_stats({ :release_cadence => 30, :average_length => 45 })
     expect(time).to eq(10)
   end
 end
