2e29b2457507dea3a7286d18b3f9687b81093e9a
Scott Muc <scottmuc@gmail.com>
2014-11-15 00:04:34 +0530

    inlining weekly_time spec code

--- spec/service/weekly_time_spec.rb
+++ spec/service/weekly_time_spec.rb
@@ -5,16 +5,14 @@ describe WeeklyTime do
     expect(FeedStats).to receive(:for) { stats }
   end
 
-  let(:time) { WeeklyTime.for "some feed" }
-
   it "reports around 60 minutes for the average podcast" do
     stub_feed_stats({ :release_cadence => 7, :average_length => 60 })
-    expect(time).to eq(60)
+    expect(WeeklyTime.for double).to eq 60
   end
 
   it "reports little time for infrequently released podcasts" do
     stub_feed_stats({ :release_cadence => 30, :average_length => 45 })
-    expect(time).to eq(10)
+    expect(WeeklyTime.for double).to eq 10
   end
 end
 
