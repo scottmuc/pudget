6dec1522e6fa6befbff1148d244dc6686fcadebc
Scott Muc <scottmuc@gmail.com>
2014-11-28 11:58:07 +0530

    really ugly implementation to dispaly opml timing

--- app/pudget.rb
+++ app/pudget.rb
@@ -1,23 +1,37 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 require_relative 'domain/podcast'
+require_relative 'domain/podcasts'
 require_relative 'services/weekly_time'
 
 class Pudget < Sinatra::Base
+  configure {
+    enable :logging
+  }
+
   get '/' do
     erb :index
   end
 
   get '/timing' do
     url = params[:url]
+    isOpml = params[:isOpml]
+    logger.info "isOpml = #{isOpml}"
+
     @dto = {
       :feed_url => url,
       :success => true
     }
     begin
-      podcast = Podcast.fetch_rss url
-      p podcast
-      @dto[:time] = WeeklyTime.for podcast
+      if isOpml == "on"
+        podcasts = Podcasts.fetch_opml url
+        @dto[:time] = podcasts.all.reduce(0) do |sum, podcast|
+          sum = sum + WeeklyTime.for(podcast)
+        end
+      else
+        podcast = Podcast.fetch_rss url
+        @dto[:time] = WeeklyTime.for podcast
+      end
     rescue Exception => e
       p e
       @dto[:success] = false
