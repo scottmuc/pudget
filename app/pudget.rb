b7585e3babc6e5e20a5f7ad1af6ee15ce40e8965
Scott Muc <scottmuc@gmail.com>
2014-12-06 11:35:37 +0530

    breaking out service call stuff from web app

--- app/pudget.rb
+++ app/pudget.rb
@@ -3,49 +3,49 @@ require "sinatra/reloader" if development?
 require_relative 'domain/podcast'
 require_relative 'domain/podcasts'
 
-class Pudget < Sinatra::Base
-  configure do
-    enable :logging
-  end
 
-  get '/' do
-    erb :index
-  end
-
-  def handle_url(&block)
-    begin
-      block.call
-    rescue Exception => e
-      logger.error e.inspect
-      { :success => false }
-    end
-  end
-
-  def get_podcasts(opml, url)
-    if opml == "on"
-      handle_url do
-        podcasts = Podcasts.fetch_opml url
-        { :podcasts => podcasts }
+module Pudget
+  class API
+    def self.handle_url(&block)
+      begin
+        block.call
+      rescue Exception => e
+        { :success => false }
       end
-    else
-      handle_url do
-        podcast = Podcast.fetch_rss url
-        podcasts = Podcasts.new [podcast]
-        { :podcasts => podcasts}
+    end
+
+    def self.get_podcasts(opml, url)
+      if opml == "on"
+        handle_url do
+          podcasts = Podcasts.fetch_opml url
+          { :podcasts => podcasts }
+        end
+      else
+        handle_url do
+          podcast = Podcast.fetch_rss url
+          podcasts = Podcasts.new [podcast]
+          { :podcasts => podcasts}
+        end
       end
     end
   end
 
-  get '/timing' do
-    url = params[:url]
-    opml = params[:isOpml]
-    view_data = { :feed_url => url,
-                  :success => true }.merge! get_podcasts(opml, url)
-    erb :search, :locals => view_data
-  end
+  class App < Sinatra::Base
+    get '/' do
+      erb :index
+    end
 
-  post '/pudget/create' do
+    get '/timing' do
+      url = params[:url]
+      opml = params[:isOpml]
+      api_result = Pudget::API.get_podcasts(opml, url)
+      view_data = { :feed_url => url, :success => true }.merge! api_result
+      erb :search, :locals => view_data
+    end
 
+    post '/pudget/create' do
+
+    end
   end
 end
 
