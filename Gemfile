0540a1946c8833511f37a0f39968f3d3c0af35f2
Scott Muc <scottmuc@gmail.com>
2017-11-11 22:04:29 +0000

    Big ol dependency update for ruby 2.4

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.1.4'
+ruby '2.4.2'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,4 @@
-require 'codeclimate-test-reporter'
 require 'hashie/mash'
-
-CodeClimate::TestReporter.start
 
 def stub_simple_rss(rss_hash)
   Hashie::Mash.new(rss_hash)
